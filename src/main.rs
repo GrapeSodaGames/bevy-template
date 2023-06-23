85d9f3fd25903236d984c3d7ca2c3f42c187a157
Josh Lindhardt <zaphodb2002@gmail.com>
2023-06-23 15:00:25 -0700

    window options

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin},
+    window::{CursorGrabMode, PresentMode, PrimaryWindow, Cursor}, ecs::schedule::OrElse,
+};
+use bevy_fly_camera::{FlyCamera, FlyCameraPlugin};
 
 fn main() {
     App::new()
-    .add_plugins(DefaultPlugins)
+    .add_plugins(DefaultPlugins
+        .set(WindowPlugin {
+            primary_window: Some(Window {
+                present_mode: PresentMode::AutoVsync,
+                mode: bevy::window::WindowMode::BorderlessFullscreen, 
+                title: "Bevy GameDev Template".to_string(),  
+                resizable: false, 
+                ..default()
+            }),
+            ..Default::default()
+    }))
+    .add_plugin(DebugStuff)
+    .add_plugin(FlyCameraPlugin)
     .add_plugin(Setup)
     .run();
 }
@@ -11,15 +28,18 @@ fn main() {
 #[derive(Component)]
 struct Thing;
 
-#[derive(Component)]
-struct Position {x: f32, y:f32, z: f32}
-
 // Resources
 #[derive(Resource)]
 struct TestTimer(Timer);
 
+#[derive(Resource)]
+struct PlayerInputs {
+    mouse_locked: bool,
+
+}
+
 // Systems
-fn add_test_entity(
+fn add_plane(
     mut commands : Commands,
     mut meshes : ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
@@ -39,7 +59,8 @@ fn add_camera(mut commands : Commands){
         ..default()
     };
 
-    commands.spawn(camera);
+    commands.spawn(camera)
+    .insert(FlyCamera::default());
 }
 
 fn add_cube(
@@ -53,15 +74,7 @@ fn add_cube(
         transform: Transform::from_xyz(0.0, 0.5, 0.0),
         ..default()
     };
-    commands.spawn(cube);
-}
-
-fn read_test_entity(time: Res<Time>, mut timer: ResMut<TestTimer>, query: Query<&Position, With<Thing>>){
-    if timer.0.tick(time.delta()).just_finished(){
-        for pos in &query{
-            println!("x = {}, y = {}, z = {} ", pos.x, pos.y, pos.z)
-        }
-    }
+    commands.spawn((Thing, cube));
 }
 
 fn add_light(
@@ -80,16 +93,83 @@ fn add_light(
     commands.spawn(light);
 }
 
+fn get_things_pos(time: Res<Time>, mut timer: ResMut<TestTimer>, query: Query<&Transform, With<Thing>>){
+    if timer.0.tick(time.delta()).just_finished(){
+        for pos in &query{
+            println!("{} ", pos.translation)
+        }
+    }
+}
+
+
+
+fn toggle_vsync(mut window_query: Query<&mut Window, With<PrimaryWindow>>,){
+    if let Ok(mut window) = window_query.get_single_mut(){
+        window.present_mode = if matches!(window.present_mode, PresentMode::AutoVsync){
+            PresentMode::AutoNoVsync
+        } else {
+            PresentMode::AutoVsync
+        };
+        info!("PRESENT_MODE: {:?}", window.present_mode )
+    }
+}
+
+fn toggle_cursor(mut window_query: Query<&mut Window, With<PrimaryWindow>>, inputs: Res<PlayerInputs>) {
+    if let Ok(mut window) = window_query.get_single_mut(){
+        info!("{}",window.title);
+        if inputs.mouse_locked {
+            window.cursor.grab_mode = CursorGrabMode::Locked;
+            window.cursor.visible = false;
+        } else {
+            window.cursor.grab_mode = CursorGrabMode::None;
+            window.cursor.visible = true;
+        }
+        
+        
+    } else {
+        info!("No primary window found.");
+    }
+}
+
+fn handle_input(keys: Res<Input<KeyCode>>, mut opt: ResMut<PlayerInputs>){
+    if keys.just_pressed(KeyCode::M){
+        opt.mouse_locked = !opt.mouse_locked;
+    }
+}
+    
+
 // Plugins
 pub struct Setup;
 impl Plugin for Setup {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(add_test_entity)
+        app.add_startup_system(add_plane)
         .add_startup_system(add_camera)
         .add_startup_system(add_cube)
         .add_startup_system(add_light)
+        .insert_resource(PlayerInputs {
+            mouse_locked: false
+        })
         .insert_resource(TestTimer(Timer:: from_seconds(2.0, TimerMode::Repeating)))
-        .add_system(read_test_entity);
+        .add_system(handle_input)
+        .add_system(toggle_cursor)
+        .add_system(get_things_pos)
+        ;
         
     }
 }
+pub struct DebugStuff;
+impl Plugin for DebugStuff{
+    fn build(&self, app: &mut App){
+        app.add_plugin(LogDiagnosticsPlugin::default())
+        .add_plugin(FrameTimeDiagnosticsPlugin)
+        .add_system(toggle_vsync);
+
+    }
+}
+// pub struct GraphicsStuff;
+// impl Plugin for GraphicsStuff{
+//     fn build(&self, app: &mut App) {
+//         app.add_system(toggle_vsync)
+//         .run();
+//     }
+// }
